72034362ef5ec2fbf23901aea185535ef05d6bd6
csfx-py <csfxshreyas@gmail.com>
2021-04-05 04:03:48 +0530

    added router and set up home route base

--- App/App.js
+++ App/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { Home } from "./Components";
 
 const App = () => {
-  return <div>test</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
