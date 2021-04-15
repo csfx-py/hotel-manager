59e0e23023038b8307ddeb7b96d74f8ca181ddc4
csfx-py <csfxshreyas@gmail.com>
2021-04-16 04:14:27 +0530

    created empty menu, table and settings component and added routing in home

--- App/Components/Home/Home.js
+++ App/Components/Home/Home.js
@@ -1,7 +1,15 @@
 import React from "react";
-import { HashRouter as Router, Switch, Redirect } from "react-router-dom";
-import SideBarItems from "./SideBarItems";
+import {
+  HashRouter as Router,
+  Switch,
+  Redirect,
+  Route,
+} from "react-router-dom";
 import { HomeContainer, SideBar } from "./HomeElements";
+import SideBarItems from "./SideBarItems";
+import Table from "./Table";
+import Settings from "./Settings";
+import Menu from "./Menu";
 
 const Home = (props) => {
   return (
@@ -12,7 +20,17 @@ const Home = (props) => {
           <SideBar>
             <SideBarItems handleLogout={props.handleLogout} />
           </SideBar>
-          <Switch></Switch>
+          <Switch>
+            <Route exact path="/home/table">
+              <Table />
+            </Route>
+            <Route exact path="/home/menu">
+              <Menu />
+            </Route>
+            <Route exact path="/home/settings">
+              <Settings />
+            </Route>
+          </Switch>
         </Router>
       </HomeContainer>
     </>
