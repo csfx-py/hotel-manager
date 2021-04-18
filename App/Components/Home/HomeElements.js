b7de191dabb9d7c1ab3872da40489cbd907aeddc
csfx-py <csfxshreyas@gmail.com>
2021-04-18 07:25:07 +0530

    corrected menu item table overflow style

--- App/Components/Home/HomeElements.js
+++ App/Components/Home/HomeElements.js
@@ -22,9 +22,11 @@ export const SideBar = styled.div`
 export const HomeMain = styled.div`
   display: flex;
   flex-direction: column;
-  flex: 1 0 auto;
+  width: calc(100vw - 100px);
+  height: 100vh;
 `;
 
+// common-------------------------------------------------------
 export const Form = styled.form`
   display: flex;
   align-items: center;
@@ -76,32 +78,10 @@ export const Button = styled.button`
   }
 `;
 
-export const SideButton = styled.button`
-  border: none;
-  border-radius: 5px;
-  color: #fff;
-  background: #43b581;
-  font-size: 1rem;
-  padding: 7px;
-  width: 80px;
-  margin-bottom: 1rem;
-  display: flex;
-  flex: 0 0 auto;
-  justify-content: center;
-  align-items: center;
-  &:hover {
-    background: rgba(67, 181, 129, 0.9);
-  }
-  &:focus {
-    outline: none;
-  }
-`;
-
 export const Table = styled.table`
   font-family: arial, sans-serif;
   border-collapse: collapse;
-  width: calc(100vw - 100px);
-  flex: 1 0 auto;
+  width: 100%;
   color: #fff;
   table-layout: fixed;
 `;
@@ -128,9 +108,20 @@ export const TableRow = styled.tr`
     background: #202225;
   }
 `;
-export const MenuTableContainer = styled.div`
-  display: flex;
-  flex-direction: 1 0 auto;
+
+export const TableData = styled.td`
+  text-align: left;
+  padding: 7px;
+
+  &:first-child {
+    width: 40%;
+    text-align: left;
+  }
+
+  &:last-child {
+    text-align: center;
+    width: 5%;
+  }
 `;
 
 export const CloseButton = styled.button`
@@ -153,17 +144,93 @@ export const CloseButton = styled.button`
   }
 `;
 
-export const TableData = styled.td`
-  text-align: left;
-  padding: 7px;
+export const SuggestDiv = styled.div`
+  position: absolute;
+  top: 90%;
+  left: 0;
+  max-height: 150px;
+  width: 100%;
+  padding: 4px;
+  background: #2f3136;
+  border-radius: 5px;
+  overflow-y: auto;
+  overflow-x: hidden;
+  z-index: 99;
+  box-shadow: 5px 5px 20px 2px rgba(0, 0, 0, 0.75);
 
-  &:first-child {
-    width: 40%;
-    text-align: left;
+  &::-webkit-scrollbar {
+    width: 4px;
   }
 
-  &:last-child {
-    text-align: center;
-    width: 5%;
+  /* Track */
+  &::-webkit-scrollbar-track {
+    background: #202225;
+    border-radius: 25px;
+  }
+  /* Handle */
+  &::-webkit-scrollbar-thumb {
+    background: #7289da;
+    border-radius: 25px;
+  }
+`;
+
+export const SuggestList = styled.ul``;
+
+export const SuggestListItem = styled.li`
+  padding: 4px;
+  border-radius: 2px;
+  color: #99aab5;
+  display: flex;
+  justify-content: space-between;
+
+  &:hover {
+    background: #202225;
+    color: #fff;
+  }
+`;
+
+// sidebar-------------------------------------------------------
+export const SideButton = styled.button`
+  border: none;
+  border-radius: 5px;
+  color: #fff;
+  background: #43b581;
+  font-size: 1rem;
+  padding: 7px;
+  width: 80px;
+  margin-bottom: 1rem;
+  display: flex;
+  flex: 0 0 auto;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    background: rgba(67, 181, 129, 0.9);
+  }
+  &:focus {
+    outline: none;
+  }
+`;
+
+// menu-------------------------------------------------------
+export const MenuTableContainer = styled.div`
+  display: flex;
+  flex-direction: 1 0 auto;
+  height: calc(100vh -200px);
+  overflow-y: auto;
+  overflow-x: hidden;
+
+  &::-webkit-scrollbar {
+    width: 4px;
+  }
+
+  /* Track */
+  &::-webkit-scrollbar-track {
+    background: #202225;
+    border-radius: 25px;
+  }
+  /* Handle */
+  &::-webkit-scrollbar-thumb {
+    background: #7289da;
+    border-radius: 25px;
   }
 `;
